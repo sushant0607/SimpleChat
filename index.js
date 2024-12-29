ed568a73fcd45a43f597df0505002c3a110d4202
Sushant Dhoble <sushantdhoble67@gmail.com>
2024-12-29 20:53:25 +0530

    Model create kar diya bhai

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chats.js")
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -17,6 +18,18 @@ async function main() {
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+let chat1 = new Chat({
+    from: "Rahul",
+    to: "john",
+    msg: "mera paisa wapas karde bhai",
+    created_at: new Date()
+});
+
+chat1.save().then((res) => {
+    console.log(res);
+});
+
 
 app.get("/", (req, res)=>{
     res.send("root chal raha hai bhai");
