f5813ac1b4027929d8d6340a6af53411f9281e37
Sushant Dhoble <sushantdhoble67@gmail.com>
2025-01-02 12:36:32 +0530

    Modified all files to create edit route and included the method-override package

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
-const Chat = require("./models/chats.js")
+const Chat = require("./models/chats.js");
+const methodOverride = require("method-override"); //to use put and patch requests
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public"))); //public folder use karne ke liye
 app.use(express.urlencoded({extended: true})); //url se directly data lene ke liye
+app.use(methodOverride("_method"));
 
 main()
 .then(() => {
@@ -53,6 +55,31 @@ app.post("/chats", (req, res) => {
   });
   res.redirect("/chats");
 });
+
+
+//edit route
+app.get("/chats/:id/edit",async (req,res) => {
+  let { id } = req.params;
+  let chat = await Chat.findById(id); //database se kuch bhi chedkaani karni ho toh async await use karna hota hai
+  res.render("edit.ejs", { chat });
+});
+
+
+//Update route
+app.put("/chats/:id", async (req,res) => {
+  let { id } = req.params;
+  let { msg: newMsg } = req.body; // to rename the content of message as newMsg
+  let updatedChat = await Chat.findByIdAndUpdate( 
+    id,
+    {msg: newMsg}, 
+    {runValidators: true, new: true}
+  );
+
+  console.log(updatedChat);
+  res.redirect("/chats");
+});
+
+
 
 app.get("/", (req, res)=>{
     res.send("root chal raha hai bhai");
